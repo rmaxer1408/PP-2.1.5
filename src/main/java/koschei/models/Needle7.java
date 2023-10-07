379cdd643f34e3b192f00d883c38b8eec3650bb6
Roman Osipov <rmnosipov59@gmail.com>
2023-10-07 15:35:53 +0300

    feat: add Task-2.1.5

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    @Autowired
+    Death8 death;
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + death.toString();
     }
 }
